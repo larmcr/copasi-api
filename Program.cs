ac119edecd619c294d38e641568252191eba092d
Luis Andres Rojas Matey <larmcr@gmail.com>
2020-11-30 21:28:50 -0600

    Third attempt

--- Program.cs
+++ Program.cs
@@ -43,35 +43,18 @@ namespace CopasiApi
       scanTask.getReport().setAppend(false);
 
       scanProblem.setSubtask(CTaskEnum.Task_steadyState);
+      scanProblem.addScanItem(CScanProblem.SCAN_LINEAR, 4);
+
       CCopasiParameterGroup scanItems = scanProblem.getGroup("ScanItems");
-      scanItems.addGroup("ScanItem");
-
       CCopasiParameterGroup scanItem = scanItems.getGroup("ScanItem");
-      
-      scanItem.addParameter("Object", CCopasiParameter.Type_CN);
-      scanItem.getParameter("Object").setCNValue(new CRegisteredCommonName("CN=Root,Model=NoName,Vector=Values[Cgh_ETS1],Reference=InitialValue"));
-      
-      scanItem.addParameter("Minimum", CCopasiParameter.Type_DOUBLE);
+
+      CRegisteredCommonName cn = new CRegisteredCommonName("CN=Root,Model=NoName,Reference=InitialValue");
+      scanItem.getParameter("Object").setCNValue(cn);
       scanItem.getParameter("Minimum").setDblValue(1.0);
-
-      scanItem.addParameter("Maximum", CCopasiParameter.Type_DOUBLE);
       scanItem.getParameter("Maximum").setDblValue(5.0);
-
-      scanItem.addParameter("log", CCopasiParameter.Type_BOOL);
       scanItem.getParameter("log").setBoolValue(false);
-
-      scanItem.addParameter("Values", CCopasiParameter.Type_STRING);
       scanItem.getParameter("Values").setStringValue("");
-
-      scanItem.addParameter("Use Values", CCopasiParameter.Type_BOOL);
       scanItem.getParameter("Use Values").setBoolValue(false);
-
-      // CCopasiParameterGroup scanItem = scanProblem.addScanItem(CScanProblem.SCAN_LINEAR, 4);
-
-      // CScanItem item;
-      // CScanMethod method;
-      // CScanProblem problem;
-      // CScanTask task;
 
       scanProblem.setContinueFromCurrentState(false);
       scanProblem.setOutputInSubtask(false);
@@ -80,20 +63,17 @@ namespace CopasiApi
       try
       {
         // scanTask.process(true);
-        model.compileIfNecessary();
+        // model.compileIfNecessary();
         dataModel.saveModel("model.cps", true);
       }
       catch
       {
         System.Console.Error.WriteLine("Error. Running the scan failed.");
         string lastErrors = scanTask.getProcessError();
-        // check if there are additional error messages
         if (!string.IsNullOrEmpty(lastErrors))
         {
-          // print the messages in chronological order
           System.Console.Error.WriteLine(lastErrors);
         }
-
         System.Environment.Exit(1);
       }
     }
