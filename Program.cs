1f74a36bdb066517c22b4112d77f39d82b867852
Luis Andres Rojas Matey <larmcr@gmail.com>
2020-12-03 18:26:18 -0600

    Add some methods (statics)

--- Program.cs
+++ Program.cs
@@ -6,74 +6,102 @@ namespace CopasiApi
 {
   class Program
   {
-    static void Main(string[] args)
+    private static void UpdateModel () 
     {
-      CDataModel dataModel = CRootContainer.addDatamodel();
       try
       {
+        CDataModel dataModel = CRootContainer.addDatamodel();
         dataModel.addModel("models/7-0/mod-00TODO-v01.cps");
+      
+        CReportDefinitionVector reports = dataModel.getReportDefinitionList();
+        CReportDefinition report = reports.createReportDefinition("Scan Parameters, Time, Concentrations, Volumes, and Global Quantity Values", "A table of scan parameters, time, variable species concentrations, variable compartment volumes, and variable global quantity values.");
+        report.setTaskType(CTaskEnum.Task_scan);
+        report.setIsTable(true);
+        report.setPrecision(6);
+        report.setSeparator(new CCopasiReportSeparator(","));
+
+        ReportItemVector table = report.getTableAddr();
+        table.Add(new CRegisteredCommonName("CN=Root,Model=NoName,Vector=Values[Cgh_ETS1],Reference=InitialValue"));
+        table.Add(new CRegisteredCommonName("CN=Root,Model=NoName,Vector=Compartments[default],Vector=Metabolites[arnPLAUR],Reference=Concentration"));
+
+        CTrajectoryTask trajectoryTask = (CTrajectoryTask)dataModel.getTask("Time-Course");
+        trajectoryTask.setMethodType(CTaskEnum.Method_stochastic);
+        trajectoryTask.getProblem().setModel(dataModel.getModel());
+        trajectoryTask.setScheduled(false);
+        CTrajectoryProblem problem = (CTrajectoryProblem)trajectoryTask.getProblem();
+        problem.setStepNumber(4);
+        dataModel.getModel().setInitialTime(0.0);
+        problem.setDuration(4);
+        problem.setTimeSeriesRequested(true);
+
+        CScanTask scanTask = (CScanTask)dataModel.getTask("Scan");
+        scanTask.setScheduled(false);
+        scanTask.getReport().setReportDefinition(report);
+        scanTask.getReport().setTarget("scan.txt");
+        scanTask.getReport().setAppend(false);
+
+        CScanProblem scanProblem = (CScanProblem)scanTask.getProblem();
+        scanProblem.setModel(dataModel.getModel());
+        scanProblem.setSubtask(CTaskEnum.Task_steadyState);
+        scanProblem.addScanItem(CScanProblem.SCAN_LINEAR, 4);
+        scanProblem.setContinueFromCurrentState(false);
+        scanProblem.setOutputInSubtask(false);
+        scanProblem.setContinueOnError(false);
+
+        CCopasiParameterGroup scanItems = scanProblem.getGroup("ScanItems");
+        CCopasiParameterGroup scanItem = scanItems.getGroup("ScanItem");
+        scanItem.getParameter("Minimum").setDblValue(1.0);
+        scanItem.getParameter("Maximum").setDblValue(5.0);
+        scanItem.getParameter("log").setBoolValue(false);
+        scanItem.getParameter("Values").setStringValue("");
+        scanItem.getParameter("Use Values").setBoolValue(false);
+
+        dataModel.saveModel("model.cps", true);
+        
+        // CRootContainer.removeDatamodel(dataModel);
       }
-      catch
+      catch (Exception exception)
       {
-        System.Console.Error.WriteLine("Error while importing the model from the given File.");
+        System.Console.Error.WriteLine("Error (UpdateModel) -> " + exception);
         System.Environment.Exit(1);
       }
-      
-      CModel model = dataModel.getModel();
-      
-      CReportDefinitionVector reports = dataModel.getReportDefinitionList();
-      
-      CReportDefinition report = reports.createReportDefinition("Scan Parameters, Time, Concentrations, Volumes, and Global Quantity Values", "A table of scan parameters, time, variable species concentrations, variable compartment volumes, and variable global quantity values.");
-      report.setTaskType(CTaskEnum.Task_scan);
-      report.setIsTable(true);
-      report.setPrecision(6);
-      report.setSeparator(new CCopasiReportSeparator(","));
+    }
 
-      ReportItemVector table = report.getTableAddr();
-      table.Add(new CRegisteredCommonName("CN=Root,Model=NoName,Vector=Values[Cgh_ETS1],Reference=InitialValue"));
-      table.Add(new CRegisteredCommonName("CN=Root,Model=NoName,Vector=Compartments[default],Vector=Metabolites[arnPLAUR],Reference=Concentration"));
+    private static void FixModel ()
+    {
 
-      CScanTask scanTask = (CScanTask)dataModel.getTask("Scan");
-      CScanProblem scanProblem = (CScanProblem)scanTask.getProblem();
-      scanProblem.setModel(dataModel.getModel());
-      scanTask.setScheduled(false);
-      
-      scanTask.getReport().setReportDefinition(report);
-      scanTask.getReport().setTarget("scan.txt");
-      scanTask.getReport().setAppend(false);
+    }
 
-      scanProblem.setSubtask(CTaskEnum.Task_steadyState);
-      scanProblem.addScanItem(CScanProblem.SCAN_LINEAR, 4);
-
-      CCopasiParameterGroup scanItems = scanProblem.getGroup("ScanItems");
-      CCopasiParameterGroup scanItem = scanItems.getGroup("ScanItem");
-
-      CRegisteredCommonName cn = new CRegisteredCommonName("CN=Root,Model=NoName,Reference=InitialValue");
-      scanItem.getParameter("Object").setCNValue(cn);
-      scanItem.getParameter("Minimum").setDblValue(1.0);
-      scanItem.getParameter("Maximum").setDblValue(5.0);
-      scanItem.getParameter("log").setBoolValue(false);
-      scanItem.getParameter("Values").setStringValue("");
-      scanItem.getParameter("Use Values").setBoolValue(false);
-
-      scanProblem.setContinueFromCurrentState(false);
-      scanProblem.setOutputInSubtask(false);
-      scanProblem.setContinueOnError(false);
-
+    private static void RunTask ()
+    {
       try
       {
-        // scanTask.process(true);
-        // model.compileIfNecessary();
-        dataModel.saveModel("model.cps", true);
+        CDataModel dataModel = CRootContainer.addDatamodel();
+        dataModel.addModel("model.cps");
+        
+        CScanTask scanTask = (CScanTask)dataModel.getTask("Scan");
+        bool processed = scanTask.process(true);
+        Console.WriteLine(processed);
+        // string key = scanTask.getKey();
+        // Console.WriteLine("key -> ", key);
       }
-      catch
+      catch (Exception exception)
       {
-        System.Console.Error.WriteLine("Error. Running the scan failed.");
-        string lastErrors = scanTask.getProcessError();
-        if (!string.IsNullOrEmpty(lastErrors))
-        {
-          System.Console.Error.WriteLine(lastErrors);
-        }
+        System.Console.Error.WriteLine("ERROR (RunTask) -> " + exception);
+        System.Environment.Exit(1);
+      }
+    }
+    public static void Main (string[] args)
+    {
+      try
+      {
+        UpdateModel();
+        // FixModel();
+        // RunTask();
+      }
+      catch (Exception exception)
+      {
+        System.Console.Error.WriteLine("ERROR (Main) -> " + exception);
         System.Environment.Exit(1);
       }
     }
