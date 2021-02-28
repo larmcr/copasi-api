fe3f029c3f69f102b75a717dd37d6c68f64a8bed
Luis Andres Rojas Matey <larmcr@gmail.com>
2021-02-27 18:45:06 -0600

    Get Experiments

--- Experiments.cs
+++ Experiments.cs
@@ -15,9 +15,6 @@ namespace CopasiApi
     private string SOURCE_MODEL = "Model.cps";
     private string RESULTS = "results";
 
-    private static string[] CNVS = new string[] { "Cgh_ETS1", "Cgh_tfAP2B1", "Cgh_tfATF1", "Cgh_tfE2F1", "Cgh_tfE2F2", "Cgh_tfE2F3", "Cgh_tfETV4", "Cgh_tfFOS", "Cgh_tfFOSL1", "Cgh_tfFOSL2", "Cgh_tfFOXP2", "Cgh_tfGATA2", "Cgh_tfJUN", "Cgh_tfJUNB", "Cgh_tfJUND", "Cgh_tfNFKB1", "Cgh_tfSMAD4", "Cgh_tfSP1", "Cgh_tfSP3", "Cgh_tfTCF7L2", "Cgh_tfTFAP2A", "Cgh_tfTFAP2C", "Cgh_tfTP53", "Cgh_MIR145", "Cgh_MIR155", "Cgh_MIR16_2", "Cgh_MIR200B", "Cgh_MIR200C", "Cgh_MIR204", "Cgh_MIR222" };
-    private static string[] SPECIES = new string[] { "arnPLAUR" };
-
     public Experiments()
     {
       var root = new DirectoryInfo(SOURCE_FOLDER);
@@ -51,24 +48,35 @@ namespace CopasiApi
       return species;
     }
 
-    private Dictionary<string, Dictionary<string, double>> GetExperiments(string [] species)
+    private Dictionary<string, Dictionary<string, string>> GetExperiments(string [] species)
     {
-      var experiments = new Dictionary<string, Dictionary<string, double>>();
+      var experiments = new Dictionary<string, Dictionary<string, string>>();
+      var listSpecies = species.ToList();
       try
       {
         var path = SOURCE_FOLDER + "/" + SOURCE_EXPERIMENTS;
         using (var parser = new TextFieldParser(path))
         {
           parser.SetDelimiters(",");
+          Dictionary<string, int> indexes = null;
           while (!parser.EndOfData) 
           {
             var row = parser.ReadFields();
-            var val = row[0];
-            var indexes = new Dictionary<string, int>();
-            if (String.IsNullOrEmpty(val)) {
+            var key = row[0];
+            if (String.IsNullOrEmpty(key)) {
               indexes = GetIndexes(species, row);
             }
+            else
+            {
+              experiments.Add(key, new Dictionary<string, string>());
+              listSpecies.ForEach((spe) =>
+              {
+                var index = indexes[spe];
+                experiments[key].Add(spe, row[index]);
+              });
+            }
           }
+          Console.WriteLine(experiments.Keys.ToArray().Length);
         }
       }
       catch (Exception exception)
@@ -85,7 +93,7 @@ namespace CopasiApi
       species.ToList().ForEach((spe) =>
       {
         var index = listHeaders.IndexOf(spe);
-        indexes[spe] = index;
+        indexes.Add(spe, index);
       });
       return indexes;
     }
